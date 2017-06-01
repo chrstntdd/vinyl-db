b8dd45cef7e3e15168242959c228e83fd13e482f
Christian Todd <christiantodd27@icloud.com>
2017-06-01 07:33:19 -0400

    add model for record.

---
+++ models/records.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const recordSchema = mongoose.Schema({
+  artist: { type: String, required: true },
+  album: { type: String, required: true },
+  releaseYear: { type: Number, required: true },
+  purchaseDate: Date,
+  genre: { type: [String], required: true },
+  rating: Number,
+  mood: [String],
+  playCount: { type: Number, default: 0 },
+  notes: String,
+  vinylColor: String,
+  accolades: [String]
+});
+
+const Record = mongoose.model('Record', recordSchema);
+
+module.exports = { Record };
