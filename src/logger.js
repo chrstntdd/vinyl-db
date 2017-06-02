bb0428a4260c8e852c025c7eb71c96b33b4ef7a5
Christian Todd <christiantodd27@icloud.com>
2017-06-02 00:06:00 -0400

    configure logger to work with tests.

--- src/logger.js
+++ src/logger.js
@@ -6,21 +6,34 @@ const consoleOptions = {
   level: 'debug',
   handleExceptions: true,
   json: true,
-  colorize: true
+  colorize: true,
+  prettyPrint: true
 };
 
-const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)(consoleOptions),
-  ]
-});
-
-logger.stream = {
-  write: (message, encoding) => {
-    logger.debug(message);
-  }
-};
-
-module.exports = {
-  logger
+// SUPRESS WINSTON OUTPUT WHEN RUNNING TESTS
+if (process.env.NODE_ENV !== 'test') {
+  let logger = new(winston.Logger)({
+    transports: [
+      new(winston.transports.Console)(consoleOptions),
+    ]
+  });
+  logger.stream = {
+    write: (message, encoding) => {
+      logger.debug(message);
+    }
+  };
+  module.exports = { logger }
+} else {
+  let logger = new(winston.Logger)({
+    transports: [
+      new(winston.transports.Console)
+    ]
+  })
+  logger.remove(winston.transports.Console);
+  logger.stream = {
+    write: (message, encoding) => {
+      logger.debug(message);
+    }
+  };
+  module.exports = { logger }
 }
