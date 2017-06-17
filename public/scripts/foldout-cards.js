9e0d79aceb9c154d58d77397aeb86ec8d237eb2c
Christian Todd <christiantodd27@icloud.com>
2017-06-17 15:10:51 -0400

    update styling.

--- public/scripts/foldout-cards.js
+++ public/scripts/foldout-cards.js
@@ -1,37 +1,32 @@
 $(document).ready(function(){
-  var zindex = 10;
-  $('.cards').on('click', '.card', function(e){
+  let zindex = 10;
+  $('.cards').on('click', '.toggle-info', function(e){
     e.preventDefault();
 
-    var isShowing = false;
+    let isShowing = false;
+    
+    let parentCard = $(this).parentsUntil('card');
 
-    if ($(this).hasClass('show')) {
+    if (parentCard.hasClass('show')) {
       isShowing = true
     }
 
-    if ($('div.cards').hasClass('showing')) {
+    if (parentCard.hasClass('showing')) {
       // a card is already in view
-      $('div.card.show')
-        .removeClass('show');
+      $('div.card.show').removeClass('show');
 
       if (isShowing) {
         // this card was showing - reset the grid
-        $('div.cards')
-          .removeClass('showing');
+        $('div.cards').removeClass('showing');
       } else {
         // this card isn't showing - get in with it
-        $(this)
-          .css({zIndex: zindex})
-          .addClass('show');
+        parentCard.css({zIndex: zindex}).addClass('show');
       }
       zindex++;
     } else {
       // no cards in view
-      $('div.cards')
-        .addClass('showing');
-      $(this)
-        .css({zIndex:zindex})
-        .addClass('show');
+      $('div.cards').addClass('showing');
+      parentCard.css({zIndex:zindex}).addClass('show');
       zindex++;
     }
   });
