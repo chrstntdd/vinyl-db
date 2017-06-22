92a47d076db2514fdb18948081b24ccfe2840d90
Christian Todd <christiantodd27@icloud.com>
2017-06-21 21:40:36 -0400

    reinforce login and sign up strategies. add webpack and try to configure :X

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,7 @@
-const passport = require('passport');
 const { User } = require('../models/user');
-var LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
-module.exports = function (passport) {
-
+module.exports = (passport) => {
   // CONFIGURATION AND SETTINGS
   passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -15,57 +13,113 @@ module.exports = function (passport) {
     });
   });
 
+  // - - - - - - - - - - - - - - - - - - - - - - - - -
   // LOCAL SIGNUP
-
+  // - - - - - - - - - - - - - - - - - - - - - - - - -
   passport.use('local-signup', new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true,
-    },
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true,
+  },
     (req, email, password, done) => {
-      process.nextTick(() => User.findOne({
-        email: email
-      }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+      // VALIDATE WITH ERROR MESSAGES
+      req.checkBody('email', 'Please enter a valid email.').notEmpty().isEmail();
+      req.checkBody('password', 'Please enter a valid password that\'s at least 6 characters long.').notEmpty().isAlpha().isLength( {min: 6 });
+
+      // SANITIZE
+      req.sanitizeBody('email').normalizeEmail({
+        all_lowercase: true,
+        gmail_remove_dots: false,
+      });
+      req.sanitizeBody('email').escape();
+      req.sanitizeBody('email').trim();
+      req.sanitizeBody('password').escape();
+      req.sanitizeBody('password').trim();
+
+      // ASSIGN SANITIZED BODY VALUES BACK TO VARS
+      email = req.body.email;
+      password = req.body.password;
+
+      process.nextTick(() => User.findOne({ email: email }, (err, user) => {
         if (user) {
-          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-        } else {
-          var newUser = new User();
+          // IF THE EMAIL ALREADY EXISTS IN DATABASE
+          return done(null, false, req.flash('signupMessage', 'Nice try, but that email is already taken.'));
+        }
+
+        req.asyncValidationErrors().then(() => {
+          // NO ERRORS. CREATE NEW USER
+          const newUser = new User();
           newUser.email = email;
           newUser.password = newUser.generateHash(password);
-          newUser.save((err) => {
+          newUser.save(() => {
             if (err) {
               throw err;
             }
-            console.log(newUser);
             return done(null, newUser);
           });
-        };
+        }).catch((errors) => {
+          // CATCH ALL ERRORS THEN FLASH TO USER IN SIGNUP VIEW
+          if (errors) {
+            const messages = [];
+            errors.forEach(error => messages.push(error.msg));
+            return done(null, false, req.flash('signupMessage', messages));
+          }
+          return done();
+        });
       }));
     }));
 
+  // - - - - - - - - - - - - - - - - - - - - - - - - -
   // LOCAL LOGIN
-
+  // - - - - - - - - - - - - - - - - - - - - - - - - -
   passport.use('local-login', new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true,
-    },
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true,
+  },
     (req, email, password, done) => {
-      User.findOne({ email: email }, (err, user) => {
-        if (err) {
-          return done(err);
-        };
-        if (!user) {
-          return done(null, false, req.flash('loginMessage', 'Looks like you don\'t have an account with us. Please sign up.' ));
-        };
-        if (!user.validPassword(password)) {
-          return done(null, false, req.flash('loginMessage', 'Your password looks a bit off. Please try again.' ));
-        };
-        console.log('Successfully logged in');
-        return done(null, user);
+      // VALIDATE WITH ERROR MESSAGES
+      req.checkBody('email', 'Please enter a valid email.').notEmpty().isEmail();
+      req.checkBody('password', 'Please enter a valid password.').notEmpty().isAlpha();
+
+      // SANITIZE
+      req.sanitizeBody('email').normalizeEmail({
+        all_lowercase: true,
+        gmail_remove_dots: false,
       });
+      req.sanitizeBody('email').escape();
+      req.sanitizeBody('email').trim();
+      req.sanitizeBody('password').escape();
+      req.sanitizeBody('password').trim();
+
+      // ASSIGN SANITIZED BODY VALUES BACK TO VARS
+      email = req.body.email;
+      password = req.body.password;
+
+      req.asyncValidationErrors().then(() => {
+          // NO ERRORS. VALIDATE EMAIL AND PASSWORD AGAINST DATABASE.
+        User.findOne({ email: email }, (err, user) => {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, req.flash('loginMessage', 'Looks like you don\'t have an account with us. Please sign up.'));
+          }
+          if (!user.validPassword(password)) {
+            return done(null, false, req.flash('loginMessage', 'Your password looks a bit off. Please try again.'));
+          }
+          // SUCCESSFUL LOGIN!
+          return done(null, user);
+        });
+      })
+        .catch((errors) => {
+          // CATCH ALL ERRORS THEN FLASH TO USER IN LOGIN VIEW
+          if (errors) {
+            const messages = [];
+            errors.forEach(error => messages.push(error.msg));
+            return done(null, false, req.flash('loginMessage', messages));
+          }
+          return done();
+        });
     }));
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,77 @@
-var path = require('path');
+const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'styles.css'
+})
 
 module.exports = {
-  cache: true,
-  entry: ['babel-polyfill', './public/app.js'],
+  entry: './src/public/scripts/app',
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    // publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use:[
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.pug$/,
+        loaders: ['file-loader?name=[path][name].html', 'pug-html-loader?pretty&exports=false'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false,
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
     }),
-  ],
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    }],
-  }
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: './src/public/views/index.pug',
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          [autoprefixer],
+        ]
+      }
+    })
+  ]
 };
